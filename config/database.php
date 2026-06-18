d16c7275621698e9c2f356ff086c47281e450694
douniaelghazi <douniaelghazi02@gmail.com>
2026-06-18 16:07:53 +0100

    connexion de la base de donnees

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$host     = "localhost";
+$dbname   = "gestion_scolaire_schoolmanager";
+$username = "root";
+$password = "";  
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+       echo("connexion reussite" ); 
+} catch (PDOException $e) {
+    echo("❌ Erreur de connexion : " . $e->getMessage());
+}
+?>
